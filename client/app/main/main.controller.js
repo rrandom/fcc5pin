b8983df9aad1dac31d7b7d47838438483ca61446
Rrandom <emanonhere@gmail.com>
2015-10-10 22:24:48 +0800

    main page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,21 +2,10 @@
 
 angular.module('fcc5pinApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+    $scope.pins = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+    $http.get('/api/pins').success(function(pins) {
+      $scope.pins = pins;
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
   });
